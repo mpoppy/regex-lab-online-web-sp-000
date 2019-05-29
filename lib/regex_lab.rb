7b21d68bb766d73de50aab1c6a1f467fc8056a7c
Marc Poppy <poppy.marc@gmail.com>
2019-05-29 04:01:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,13 @@
 def starts_with_a_vowel?(word)
-
+   word.match(/\A[aeiou]/i) != nil ? true : false
 end
 
+#returns an arrary starting with un and ending with ing in the same word
 def words_starting_with_un_and_ending_with_ing(text)
-
+  
 end
 
+#return array with words 5 letters long
 def words_five_letters_long(text)
 
 end
