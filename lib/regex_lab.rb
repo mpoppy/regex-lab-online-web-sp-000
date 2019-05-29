dde91ba2c52e645e02faa297b4af2050032094fc
Marc Poppy <poppy.marc@gmail.com>
2019-05-29 04:04:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ end
 
 #returns an arrary starting with un and ending with ing in the same word
 def words_starting_with_un_and_ending_with_ing(text)
-  text.grep(/pattern/) { |match|  }
+  
 end
 
 #return array with words 5 letters long
